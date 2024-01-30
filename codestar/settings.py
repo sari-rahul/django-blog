4110134448993c62e2e61795a7e792357b3c60e9
sari-rahul <>
2024-01-30 13:26:38 +0000

    add Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['8000-sari-rahul-django-blog-fco2uou6g2.us2.codeanyapp.com','my-django-blogg-8b318e9a4b3c.herokuapp.com']
 
 
@@ -87,6 +87,10 @@ WSGI_APPLICATION = "codestar.wsgi.application"
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
