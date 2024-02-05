dd80a307b8b50ee6d7adfd91aa51cfd3c350f5e5
Sarimol Adampurath <sarisivadas93@gmail.com>
2024-02-05 12:20:18 +0000

    add authentication

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['8000-sarirahul-djangoblog-wmjkqfizqbd.ws-eu107.gitpod.io','my-django-blogg-8b318e9a4b3c.herokuapp.com']
+ALLOWED_HOSTS = ['8000-sarirahul-djangoblog-wmjkqfizqbd.ws-eu108.gitpod.io','my-django-blogg-8b318e9a4b3c.herokuapp.com']
 
 
 # Application definition
@@ -43,10 +43,18 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
     "django_summernote",
     "blog",
     "about",
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -57,6 +65,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = "codestar.urls"
@@ -116,6 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
