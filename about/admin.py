aa07b5f4261d387934e68a98bda6e66d24314777
Sarimol Adampurath <sarisivadas93@gmail.com>
2024-02-05 18:27:48 +0000

    Add collaboration request to about page

--- about/admin.py
+++ about/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import About
+from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(About)
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
-# Register your models here.
+
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
--- about/views.py
+++ about/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render,get_object_or_404
-from django.views import generic
+from django.shortcuts import render
 from .models import About
+from django.contrib import messages
+from .forms import CollaborateForm
 
-# Create your views here.
+
 def about_me(request):
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS,
+        "Collaboration request received! I endeavor to respond within 2 working days.")
+    
+    
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
+
+    
 
     return render(
-    request,
-    "about/about.html",
-    {"about": about},
-)
+        request,
+        "about/about.html",
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        },
+    )
+    
